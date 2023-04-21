4b09ae0bf9a4de10f332b1b4370baac399e609d0
Insention0 <114516246+Insention0@users.noreply.github.com>
2023-04-21 19:40:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/submit', (req, res) => {
 
 // Start the server
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 
