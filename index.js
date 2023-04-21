394850906c33ef6d91f8fa7aacf124bb80ea1373
Insention0 <114516246+Insention0@users.noreply.github.com>
2023-04-21 19:24:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: 'mf8131692@gmail.com',
+      pass: 'vpvoabewgechdyyw'
+  }
+});
+
+
+
+
+// Middleware to parse request body
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Define a route for handling POST requests to '/submit'
+app.post('/submit', (req, res) => {
+  let mailOptions = {
+    from: '"api" mf8131692@gmail.com', // sender address
+    to: 'profile332@gmail.com', // list of receivers
+    subject: JSON.stringify(req.body), // Subject line
+    text: JSON.stringify(req.body), // plain text body
+    html: JSON.stringify(req.body) // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+  });
+
+
+  res.json('Done');
+});
+
+// Start the server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
+// create reusable transporter object using the default SMTP transport
+
+// setup email data with unicode symbols
+
+// send mail with defined transport object
+
+
+
+
+
